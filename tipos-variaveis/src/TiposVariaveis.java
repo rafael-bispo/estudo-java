23a762f03a5bfc377635b3993bb509a910231e40
rafael-bispo <rafaeldossantosbispo@gmail.com>
2023-01-25 20:02:12 -0300

    Estudo de tipos de variáveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,11 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // TiposEVariaveis.java
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal; //usando casting para corrigir tipo de variáveis. No caso, a melhor saída seria colocar todas como int
+
+        //declarando constante
+        final double VALOR_DE_PI = 3.14;
     }
 }
