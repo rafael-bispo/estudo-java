33f434c0a767f3f44b8f058c728ffebe052e16e6
rafael-bispo <rafaeldossantosbispo@gmail.com>
2023-01-25 20:26:36 -0300

    Exemplo 1 do estudo de operadores

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,31 @@
+public class Operadores {
+    public static void main(String[] args) {
+        String nomeCompleto = "LINGUAGEM " + "JAVA";
+        System.out.print(nomeCompleto);
+
+        /*Fazendo debug do trecho de código abaixo é possível perceber que o operador + realiza soma até achar um string, depois de achar string ele concatena mesmo que volte a aparecer valores tipo int. Nesse caso, importa a precedência*/
+        String concatenacao ="?";
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+1+1+"1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+"1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+1+1+1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+(1+1+1);
+
+        System.out.println(concatenacao);
+    }
+}
