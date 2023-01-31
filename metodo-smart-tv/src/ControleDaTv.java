33f28e185bfb25a69d4a8a832be1534e94885ff8
rafael-bispo <rafaeldossantosbispo@gmail.com>
2023-01-31 19:55:27 -0300

    Mudança na documentação para torná-la mais clara

--- metodo-smart-tv/src/ControleDaTv.java
+++ metodo-smart-tv/src/ControleDaTv.java
@@ -1,3 +1,14 @@
+/**
+* <h1>SmartTv</h1>
+* Este é um exemplo para o entedimento relativo a classes, atributos e métodos.
+* <p>
+* <b>Note:</b> Leia atentamente a documentação desta classes
+* para desfrutar dos recursos oferecidos pelo autor
+*
+* @author  Rafael Bispo (feito como base no exemplo do curso do professor Gleyson Sampaio)
+* @version 1.1
+* @since   31/01/2023
+*/
 public class ControleDaTv {
     public static void main(String[] args) throws Exception {
        
@@ -31,9 +42,14 @@ public class ControleDaTv {
 
         exibirEstadoDaTv(tv);
     }
-    //Método para exibir o estado atual da TV
+    
+    /**
+   * Este método é utilizado para mudar o canal diretamente
+   * @param tv2 é o objeto do tipo SmartTv que recebe os parâmetros a serem exibidos
+   * @return Não retorna nenhum valor
+   */
     public static void exibirEstadoDaTv(SmartTv tv2){
-        //Exibindo o estado atual
+        
         System.out.println("\n\nEstados atuais dos atributos da TV: ");
         System.out.println("Está ligada?: "+tv2.ligada);
         System.out.println("Em qual canal está atualmente?: "+tv2.canal);
--- metodo-smart-tv/src/SmartTv.java
+++ metodo-smart-tv/src/SmartTv.java
@@ -1,33 +1,70 @@
+/**
+* <h1>SmartTv</h1>
+* Este é um exemplo para o entedimento relativo a classes, atributos e métodos.
+* <p>
+* <b>Note:</b> Leia atentamente a documentação desta classes
+* para desfrutar dos recursos oferecidos pelo autor
+*
+* @author  Rafael Bispo (feito como base no exemplo do curso do professor Gleyson Sampaio)
+* @version 1.1
+* @since   31/01/2023
+*/
 public class SmartTv {
     boolean ligada = false;
     int volume = 10;
     int canal = 1;
 
-    //Métodos que ligam e desligam a TV
+   /**
+   * Este método é utilizado para mudar o valor da variável ligada para true
+   * @return Não retorna nenhum valor
+   */
     public void ligar(){
         ligada=true;
     }
+    /**
+   * Este método é utilizado para mudar o valor da variável ligada para false
+   * @return Não retorna nenhum valor
+   */
     public void desligar(){
         ligada=false;
     }
 
-    //Método para aumentar e diminuir o volume da TV
+    /**
+   * Este método é utilizado para incrementar o valor da variável volume de 1 em 1
+   * @return Não retorna nenhum valor
+   */
     public void aumentarVolume(){
         volume++;
     }
+    /**
+   * Este método é utilizado para decrementar o valor da variável volume de 1 em 1
+   * @return Não retorna nenhum valor
+   */
     public void diminuirVolume(){
         volume--;
     }
 
-    //Método avançar ou voltar canal
+    /**
+   * Este método é utilizado para incrementar o valor da variável canal de 1 em 1
+   * @return Não retorna nenhum valor
+   */
     public void avancarCanal(){
         canal++;
     }
+    /**
+   * Este método é utilizado para decrementar o valor da variável canal de 1 em 1
+   * @return Não retorna nenhum valor
+   */
     public void voltarCanal(){
         canal--;
     }
 
-    //Método para mudar canal direto
+    /**
+   * Este método é utilizado para mudar o canal diretamente
+   * @param novoCanal este valor representa o número do canal para o qual a
+   * televisão mudará
+   * @return Não retorna nenhum valor
+   */
     public void mudarCanal(int novoCanal){
         canal=novoCanal;
     }
