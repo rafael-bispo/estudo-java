44d22a30ce9f46135aeff03e5a6e417833cd9d73
rafael-bispo <rafaeldossantosbispo@gmail.com>
2023-02-20 12:01:35 -0300

    Adicionando arquivo de teste de package

---
+++ exemplo-de-pacotes/src/nivel1/nivel2/nivel3/Main.java
@@ -0,0 +1,11 @@
+package nivel1.nivel2.nivel3;
+/*
+* Aqui em cima se declaram os pacotes. Usando a "lâmpada"
+* é possível criar automaticamente os pacotes. Além disso, o
+* jeito certo é ir em FIle/New/package e criar o pacote.
+* */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
